94cc2c8b2607276370f20be1ee47d8fb082331dd
dudleyspence <dudleyspence5@gmail.com>
2024-08-20 11:26:10 +0200

    adds CORS middleware for all origins

--- src/app.py
+++ src/app.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
 from src.controllers.google_places_controllers import router as google_places_router
-import sys
-print("PYTHONPATH:", sys.path)
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Allow all origins
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
+
+
 
 
 app.include_router(google_places_router, prefix="/api")
