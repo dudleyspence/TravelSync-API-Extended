928605e2eabaa446e14dec2c120f0ce43f236d2d
Mahdi <mahdih2105@gmail.com>
2024-08-23 15:24:39 +0100

    completed users endpoint

--- src/models/user.py
+++ src/models/user.py
@@ -7,6 +7,6 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String(100), unique=True)
     email = Column(String(200), unique=True)
-    hashed_password = Column(String(300), nullable=False)
+    password = Column(String(300), nullable=False)
     created_at = Column(DateTime, default=func.now())
 
