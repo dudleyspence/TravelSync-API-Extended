703a7d6f15fca6f69509c24a5ec6feea7de1b3dc
Mahdi <mahdih2105@gmail.com>
2024-08-23 15:06:49 +0100

    Added delete event

--- src/routers/itinerary_events.py
+++ src/routers/itinerary_events.py
@@ -7,3 +7,11 @@ from src.db.database import get_db
 router = APIRouter()
 
 
+@router.delete('/{event_id}', response_model=ItineraryEventResponse)
+def delete_event(event_id: int, db: Session = Depends(get_db)) -> ItineraryEventResponse:
+    db_event = db.query(ItineraryEvent).filter(ItineraryEvent.id == event_id).first()
+    if db_event is None:
+        raise HTTPException(status_code=404, detail='Event not found')
+    db.delete(db_event)
+    db.commit()
+    return db_event
