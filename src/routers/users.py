0afdd46feea4b7944ba17a08a4aa860e26c6a98c
dudleyspence <dudleyspence5@gmail.com>
2024-08-30 19:07:47 +0200

    fixes bug in the usersItineraries endpoint

--- src/routers/users.py
+++ src/routers/users.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from src.schemas import UserCreate, UserLogin, UserResponse, ItineraryResponse 
-from src.models import User, ItineraryMember  
+from src.models import User, ItineraryMember, Itinerary
 from src.db.database import get_db
 from typing import List
 from .utils import verify_password, hash_password
@@ -10,7 +10,7 @@ router = APIRouter()
 
 # login user
 @router.post('/user', response_model=UserResponse)
-def get_user(user: UserLogin, db: Session = Depends(get_db)) -> UserResponse:
+def get_user_for_login(user: UserLogin, db: Session = Depends(get_db)) -> UserResponse:
     db_user = db.query(User).filter(User.username == user.username).first()
     if db_user is None or not verify_password(user.password, db_user.password):
         raise HTTPException(status_code=404, detail="Invalid email or password")
@@ -18,7 +18,7 @@ def get_user(user: UserLogin, db: Session = Depends(get_db)) -> UserResponse:
 
 # add a new user
 @router.post('/', response_model=UserResponse)
-def post_user(user: UserCreate, db: Session = Depends(get_db)) -> UserResponse:
+def create_user(user: UserCreate, db: Session = Depends(get_db)) -> UserResponse:
     db_user = db.query(User).filter(User.email == user.email).first()
     if db_user:
         raise HTTPException(status_code=400, detail="Email already in use")
@@ -36,11 +36,13 @@ def post_user(user: UserCreate, db: Session = Depends(get_db)) -> UserResponse:
     return new_user
 
 # gets all itineraries for a user
-@router.get('/{user_id}/itineraries', response_model=List[ItineraryResponse])  # Updated route and response_model
+@router.get('/{user_id}/itineraries', response_model=List[ItineraryResponse])  
 def get_user_itineraries(user_id: int, db: Session = Depends(get_db)):
-    itinerary_memberships = db.query(ItineraryMember).filter(ItineraryMember.user_id == user_id).all()  # Updated query
-    
-    if not itinerary_memberships:
-        raise HTTPException(status_code=404, detail="User not found or no itineraries found")
+    itinerary_memberships = db.query(ItineraryMember).filter(ItineraryMember.user_id == user_id).all()
 
-    return itinerary_memberships  
+    #  making a loop that extracts into a list the itinerary_ids for all the itineraries this user is a member of
+    itinerary_ids = [membership.itinerary_id for membership in itinerary_memberships]
+
+    itineraries = db.query(Itinerary).filter(Itinerary.id.in_(itinerary_ids)).all()
+
+    return itineraries  
