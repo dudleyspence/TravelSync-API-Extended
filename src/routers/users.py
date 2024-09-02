eac05763e1c8ad621b60b1383be7935045091e62
dudleyspence <dudleyspence5@gmail.com>
2024-09-02 16:31:54 +0200

    adds get all user details

--- src/routers/users.py
+++ src/routers/users.py
@@ -44,7 +44,7 @@ def get_user_itineraries(user_id: str, db: Session = Depends(get_db)):
 # get all details for one user 
 @router.get('/{user_id}', response_model=UserResponse)  
 def get_user(user_id: str, db: Session = Depends(get_db)):
-    user = db.query(User).filter(User.user_id == user_id).first()
+    user = db.query(User).filter(User.id == user_id).first()
 
     if not user:
         raise HTTPException(status_code=404, detail="User not found in database")
