e55ff52d47f2fb313b57f3dca42a74b361a79260
dudleyspence <dudleyspence5@gmail.com>
2024-09-04 01:01:40 +0200

    adds the delete single file endpoint

--- src/routers/files.py
+++ src/routers/files.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException, UploadFile, File
+from fastapi import APIRouter, Depends, HTTPException, UploadFile, File, status
 import os
 from sqlalchemy.orm import Session
 from src.schemas import FileResponse
@@ -50,25 +50,22 @@ router = APIRouter()
 
 @router.post('/upload/{itinerary_id}', response_model=FileResponse)
 def upload_file(itinerary_id: int, file: UploadFile = File(...), db: Session = Depends(get_db))-> FileResponse:
-    # used a try as an error during file upload isnt unlikely
     try:    
             print(file)
             # unique ID for the file
             file_id = str(uuid.uuid4())
 
-            # get type of the file
             file_type = mimetypes.guess_type(file.filename)[0] or 'application/octet-stream'
 
-            # Create a blob object in Firebase Storage
             blob = bucket.blob(f"{file_id}/{file.filename}")
 
             # Upload the file to Storage
             blob.upload_from_file(file.file, content_type=file.content_type)
 
-            # Construct the file URL
+            # construct file URL
             file_url = f"https://storage.googleapis.com/{bucket.name}/{file_id}/{file.filename}"
 
-            # Create a new file record in the database
+            # create new file in database
             new_file = FileModel(
                 itinerary_id=itinerary_id,  
                 file_name=file.filename,
@@ -76,8 +73,6 @@ def upload_file(itinerary_id: int, file: UploadFile = File(...), db: Session = D
                 file_path=file_url
             )
 
-
-            # Add and commit the new file record to the database
             db.add(new_file)
             db.commit()
             db.refresh(new_file)
@@ -97,7 +92,23 @@ def get_itinerary_files(itinerary_id: int, db: Session = Depends(get_db)) -> Lis
 
 
 
+@router.delete('{file_id}', status_code=status.HTTP_204_NO_CONTENT)
+def remove_itinerary_file(file_id: int, db: Session = Depends(get_db)):
+    file_to_delete = db.query(FileModel).filter(FileModel.file_id == file_id).first()
+    
+    if file_to_delete is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="File not found")
 
+    try:
+        # the .replace just removes our baseURL/bucketname from the full path to only give the relative path
+        file_path = file_to_delete.file_path.replace(f"https://storage.googleapis.com/{bucket.name}/", "")
+        blob = bucket.blob(file_path)
+        blob.delete() 
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Failed to delete file from storage")
+    
+
+     
 
 # @router.delete('/delete_all/{itinerary_id}')
 # def delete_all_files(itinerary_id: int):
