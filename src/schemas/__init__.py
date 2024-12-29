ba674dd7a701dfdf527d63b4cba6ef8dae8f2919
Dudley Spence <dudleyspence5@gmail.com>
2024-12-30 00:54:11 +0100

    fixed import error

--- src/schemas/__init__.py
+++ src/schemas/__init__.py
@@ -1,5 +1,5 @@
 from .user import UserResponse, UserCreate
 from .itinerary_member import ItineraryMemberCreate, ItineraryMemberResponse 
-from .itinerary import ItineraryCreate, ItineraryResponse, ItineraryUpdate, JoinItineraryRequest 
-from .itinerary_location import ItineraryEventCreate, ItineraryEventResponse
+from .itinerary import ItineraryCreate, ItineraryResponse, ItineraryReorderRequest, JoinItineraryRequest 
+from .itinerary_location import ItineraryLocationCreate, ItineraryLocationResponse
 from .file import FileCreate, FileResponse
