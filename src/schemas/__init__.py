12f2089549d9c667a1f5401940da2d778958ce5d
Dudley Spence <dudleyspence5@gmail.com>
2024-12-31 17:32:45 +0100

    update route for add location

--- src/schemas/__init__.py
+++ src/schemas/__init__.py
@@ -1,5 +1,5 @@
 from .user import UserResponse, UserCreate
 from .itinerary_member import ItineraryMemberCreate, ItineraryMemberResponse 
 from .itinerary import ItineraryCreate, ItineraryResponse, ItineraryReorderRequest, JoinItineraryRequest 
-from .itinerary_location import ItineraryLocationCreate, ItineraryLocationResponse
+from .itinerary_location import ItineraryLocationResponse
 from .file import FileCreate, FileResponse
